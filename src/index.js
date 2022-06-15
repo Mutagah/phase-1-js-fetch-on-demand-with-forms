95ca04405ed8300acc863ff5e7bfde2dd041a945
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-06-15 09:55:00 +0300

    Added an event that would take user input,fetch data from a server and output it in our window.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-const init = () => {
-  
+const init = (event) => {
+  event.preventDefault()
+  const inputForm = document.querySelector('form')
+  inputForm.addEventListener('submit', e => {
+      e.preventDefault()
+       fetch(`http://localhost:3000/movies/${e.target.children[1].value}`)
+      .then(response => response.json())
+      .then(data => {
+          const title = document.querySelector("section#movieDetails h4");
+          const summary = document.querySelector("section#movieDetails p")
+          title.textContent = data.title
+          summary.textContent = data.summary
+      })
+      
+  } )
 }
 
 document.addEventListener('DOMContentLoaded', init);
